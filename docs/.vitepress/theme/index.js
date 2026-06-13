5d0ae505e40c1bcf296229613269212e96f34808
gggxbbb <gggxbbb@foxmail.com>
2026-06-13 21:04:08 +0800

    style: 接入小米 MiSans 字体

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,7 @@
+import DefaultTheme from 'vitepress/theme'
+import 'misans/lib/Normal/MiSans-Regular.min.css'
+import 'misans/lib/Normal/MiSans-Semibold.min.css'
+import 'misans/lib/Latin/MiSansLatin-Regular.min.css'
+import './custom.css'
+
+export default DefaultTheme
